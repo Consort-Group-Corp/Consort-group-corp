e0b5e01041494266421b94abb077d5933bc529b1
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2025-07-30 01:24:10 +0500

    added webinar request and response dtos 2.0

--- webinar_service/src/main/java/uz/consortgroup/webinar_service/entity/Webinar.java
+++ webinar_service/src/main/java/uz/consortgroup/webinar_service/entity/Webinar.java
@@ -1,11 +1,23 @@
 package uz.consortgroup.webinar_service.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-import uz.consortgroup.core.api.v1.dto.webinar.LanguageCode;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import uz.consortgroup.core.api.v1.dto.webinar.enumeration.LanguageCode;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -27,6 +39,9 @@ public class Webinar {
 
     @Column(name = "preview_url")
     private String previewUrl;
+
+    @Column(name = "preview_filename")
+    private String previewFilename;
 
     @Column(name = "start_time", nullable = false)
     private LocalDateTime startTime;
