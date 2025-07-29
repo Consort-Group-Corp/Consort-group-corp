f06629857c6aa7401467d73f3ee6163055ff30e1
Doniyor Kurbanov <doniyor.kurbanov.24@gmail.com>
2025-07-29 23:33:35 +0500

    added webinar enum

---
+++ webinar_service/src/main/java/uz/consortgroup/webinar_service/entity/Webinar.java
@@ -0,0 +1,66 @@
+package uz.consortgroup.webinar_service.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "webinars", schema = "webinar_schema")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Webinar {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(name = "preview_url")
+    private String previewUrl;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalDateTime endTime;
+
+    @Column(name = "platform_url", nullable = false)
+    private String platformUrl;
+
+    @Column(name = "course_id", nullable = false)
+    private UUID courseId;
+
+    @Column(name = "language_code", nullable = false, length = 50)
+    private String languageCode;
+
+    @Column(name = "created_by", nullable = false)
+    private UUID createdBy;
+
+    @OneToMany(mappedBy = "webinar", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<WebinarParticipant> participants;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
